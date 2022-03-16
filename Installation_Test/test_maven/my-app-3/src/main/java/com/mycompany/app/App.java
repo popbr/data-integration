74053bcf0b1e17598cf9eff30f1336a0ad507180
MNSleeper <sleepernoahspam@gmail.com>
2022-03-16 19:16:26 -0400

    my-app-3 can establish file paths, file list, and can interact with CSV files in a scanner.

--- Installation_Test/test_maven/my-app-3/src/main/java/com/mycompany/app/App.java
+++ Installation_Test/test_maven/my-app-3/src/main/java/com/mycompany/app/App.java
@@ -1,23 +1,111 @@
 package com.mycompany.app;
 
+import java.io.Console;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+
 import org.apache.commons.io.FilenameUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+
+import java.text.CharacterIterator;
+import java.util.Scanner;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
 
 public class App
 {
   public static void main(String[] args) throws Exception
   {
-    EstablishFilePath();
+    System.out.println("\n");
+    String FilePath = EstablishFilePath() + "\\Downloads\\";
+    File[] fileNames = EstablishFileList(FilePath);
+
+    for (int i = 0; i < fileNames.length; i++) {
+      System.out.println("\n"+fileNames[i].getName());
+      System.out.println(fileNames[i].getPath()+"\n");
+      Parsefromtxt(fileNames[i].getPath(), "\",\"");
+    }
+
+    
+    // writing the workbook into the file
+    //FileOutputStream out = new FileOutputStream( new File("./GFGsheet.xlsx")); //C:\Users\sleep\Desktop\Excel
+    //Wbook.write(out);
+    //out.close();
+    System.out.println("\n");
+
+  }
+
+  public static String EstablishFilePath(){
+		File s = new File("f.txt");
+		String FilePath = "";
+		char[] tempChar = s.getAbsolutePath().toCharArray();
+		char[] newChar = new char[tempChar.length - 6];
+		for (int i = 0; i < newChar.length; i++) {
+			newChar[i] = tempChar[i];
+		}
+		FilePath = String.valueOf(newChar);
+    //System.out.println(FilePath);
+		return FilePath;
+	}
+
+  public static File[] EstablishFileList(String FilePath) {
+	  File f = new File(FilePath);
+		
+		File[] fileN = f.listFiles();
+		File[] fileNames = new File[fileN.length];
+		int txtCatch = 0;
+		for (int i = 0; i < fileN.length; i++) {
+			if (!fileN[i].getName().equals("ReadMeDownloads.txt")) {
+				fileNames[txtCatch] = fileN[i];
+        System.out.println(fileNames[txtCatch].getName());
+				txtCatch++;
+			}
+		}
+		return fileNames;
+	}
+
+  public static void Parsefromtxt(String txtlocation, String Delim) throws Exception{
+    Scanner txtFile = new Scanner(new File(txtlocation));
+
+		String txtFields = txtFile.nextLine();
+		Scanner txtFieldsLine = new Scanner(txtFields);
+		txtFieldsLine.useDelimiter(Delim);
+
+    int index = 0;
+		int Limit = 9050 + 1;
+
+		while (txtFile.hasNextLine()) { 
+      /*ISSUE: for some reason, the .nextLine() quits at line 333, and cuts off that line before ends. There's 356 
+      entries in the CSV file total, so this cuts out 23 entries, for some reason. Research it further. 
+      Something's weird*/
+			index++;
+			System.out.println(txtFile.nextLine()+"\n");
+		}
+		if (index < Limit)
+			Limit = (index + 1);
+		index = 0;
+		txtFile.reset();
+    txtFile = new Scanner(new File(txtlocation));
+
+		txtFields = txtFile.nextLine();
+		txtFieldsLine = new Scanner(txtFields);
+		txtFieldsLine.useDelimiter(Delim);
+    int indexTracker = 0;
+		String currentWord = "";
+		String currentLine = "";
+    
+  }
+  
+  public static void OldTest() {
     XSSFWorkbook Wbook = new XSSFWorkbook(); // workbook object
     XSSFSheet spreadsheet = Wbook.createSheet("Research Data"); // spreadsheet object
     XSSFRow row; // creating a row object
@@ -55,18 +143,5 @@ public class App
     Wbook.write(out);
     out.close();
   }
-
-  public static String EstablishFilePath(){
-		File s = new File("f.txt");
-		String FilePath = "";
-		char[] tempChar = s.getAbsolutePath().toCharArray();
-		char[] newChar = new char[tempChar.length - 6];
-		for (int i = 0; i < newChar.length; i++) {
-			newChar[i] = tempChar[i];
-		}
-		FilePath = String.valueOf(newChar);
-    System.out.println(FilePath);
-		return FilePath;
-	}
   
 }
