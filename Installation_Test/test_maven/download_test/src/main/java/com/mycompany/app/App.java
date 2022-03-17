92d39b90b0c5b6c106fda65faa09d99115355e48
Clément Aubert <caubert@augusta.edu>
2022-03-16 22:01:28 -0400

    Added dependency in the correct file…

--- Installation_Test/test_maven/download_test/src/main/java/com/mycompany/app/App.java
+++ Installation_Test/test_maven/download_test/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,22 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.io.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+// inspired from 
+// https://www.techiedelight.com/download-file-from-url-java/
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void downloadFile(URL url, String fileName) throws IOException {
+        FileUtils.copyURLToFile(url, new File(fileName));
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        downloadFile(new URL("https://www.nsf.gov/awardsearch/download?DownloadFileName=1968&All=true"), "1968.zip");
     }
 }
+
